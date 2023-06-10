052b00d13a32124630ae7dcfd3f45e9ce5031d4d
shazidkarim <shazid747@gmail.com>
2023-06-10 18:32:44 +0600

    users show api

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ async function run() {
         const myClassCollection = client.db("summerCampDb").collection("myClass");
 
         // users relared apis
-
+        app.get('/users',async(req,res)=>{
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
         app.post('/users', async(req,res)=>{
             const user= req.body;
             const query = {email: user.email}
