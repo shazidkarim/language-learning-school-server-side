53c7b948078aa1bf0a8a97c37dc42185ccffb3f0
shazidkarim <shazid747@gmail.com>
2023-06-10 13:14:41 +0600

    fix delete api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wyepuci.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -58,6 +58,9 @@ async function run() {
         })
         app.delete('/myclass/:id', async(req,res)=>{
             const id = req.params.id;
+            const query = { _id : new ObjectId(id)};
+            const result = await myClassCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
